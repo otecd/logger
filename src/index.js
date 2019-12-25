a6cd19db6e690ed80825b8722a773a418731e5a6
Dmitrii Pron <idimzz@gmail.com>
2019-12-25 09:43:29 +0000

    fix console

--- src/index.js
+++ src/index.js
@@ -7,14 +7,13 @@ const colors = {
   white: '\x1b[37m'
 }
 const redColor = '\x1b[31m'
+const c = console
 
-export default (name, color) => {
-  return {
-    log: (msg) => {
-      console.log(`${colors[color]}${name}:\x1b[0m ${msg}`)
-    },
-    error: (msg) => {
-      console.error(`${colors[color]}${name}:${redColor}Error:\x1b[0m ${msg}`)
-    }
+export default (name, color) => ({
+  log: (msg) => {
+    c.log(`${colors[color]}${name}:\x1b[0m ${msg}`)
+  },
+  error: (msg) => {
+    c.error(`${colors[color]}${name}:${redColor}Error:\x1b[0m ${msg}`)
   }
-}
+})
